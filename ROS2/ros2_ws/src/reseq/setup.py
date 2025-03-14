323955e446d0bdd0f71d8b4e540594f323cfd713
grigiot <s336224@studenti.polito.it>
2025-03-14 20:24:57 +0100

    ros done

--- ROS2/ros2_ws/src/reseq/setup.py
+++ ROS2/ros2_ws/src/reseq/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'reseq'
 
@@ -9,7 +10,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml', 'launch/launch_file.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
